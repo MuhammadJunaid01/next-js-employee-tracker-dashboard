819bf35650ba223525255222dcd74e4bb9ce99fe
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2022-09-02 22:07:03 +0600

    fix task data modification

--- component/task/Task.jsx
+++ component/task/Task.jsx
@@ -1,6 +1,39 @@
-import React from "react";
-
+import { useEffect } from "react";
+import { useState } from "react";
+import useSWR from "swr";
+const fetcher = async (url) => {
+  const res = await fetch("http://localhost:3004/tasks");
+  const data = await res.json();
+  return data;
+};
 const Task = () => {
+  const [my, setMy] = useState([]);
+  const user = JSON.parse(localStorage.getItem("user"));
+  // const user = localStorage.getItem("user");
+  const { data, error } = useSWR("/tasks", fetcher);
+
+  useEffect(() => {
+    const mytask = [];
+    const mylead = [];
+
+    data?.map((task) => {
+      task.teamLead?.forEach((lead) => {
+        if (lead.label == user.name) {
+          mylead.push(lead);
+        }
+      });
+      task.teamMember?.forEach((member) => {
+        if (user.email.toLowerCase() === member.value.toLowerCase()) {
+          mytask.push(task);
+        }
+      });
+    });
+
+    setMy(mytask);
+  }, []);
+
+  const text = "kkmmkmk";
+  console.log("my task", my);
   return (
     <div>
       <h1>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</h1>
