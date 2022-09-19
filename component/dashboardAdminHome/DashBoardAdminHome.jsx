f1ba8056e25ecfd972b51c6fa10f98188c278bf3
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2022-09-20 00:02:22 +0600

    added something

--- component/dashboardAdminHome/DashBoardAdminHome.jsx
+++ component/dashboardAdminHome/DashBoardAdminHome.jsx
@@ -1,16 +1,27 @@
 import { UpCircleOutlined } from "@ant-design/icons";
 import React from "react";
 import SimpleBreef from "../simpleBreef/SimpleBreef";
-import { FaMemory } from "react-icons/fa";
+import { FaThLarge } from "react-icons/fa";
+import { Col, Row } from "antd";
 
 const DashBoardAdminHome = ({ Icon, title }) => {
   return (
     <div>
       <h1>hello home</h1>
-      <SimpleBreef
-        Icon={FaMemory}
-        title={"props from dashboard admin jjjjjjj"}
-      />
+      <Row gutter={16}>
+        <Col xs={24} md={6}>
+          <SimpleBreef Icon={FaThLarge} count={20} title={"Projects"} />
+        </Col>
+        <Col xs={24} md={6}>
+          <SimpleBreef Icon={FaThLarge} count={20} title={"Projects"} />
+        </Col>
+        <Col xs={24} md={6}>
+          <SimpleBreef Icon={FaThLarge} count={20} title={"Projects"} />
+        </Col>
+        <Col xs={24} md={6}>
+          <SimpleBreef Icon={FaThLarge} count={20} title={"Projects"} />
+        </Col>
+      </Row>
     </div>
   );
 };
--- component/simpleBreef/SimpleBreef.jsx
+++ component/simpleBreef/SimpleBreef.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const SimpleBreef = ({ Icon, title, count }) => {
+const SimpleBreef = ({ Icon, count, title }) => {
   // console.log((Icon = "hello icon undifine"), title, count);
 
   return (
@@ -9,30 +9,33 @@ const SimpleBreef = ({ Icon, title, count }) => {
         backgroundColor: "#16191C",
         display: "flex",
         alignItems: "center",
-        justifyContent: "space-around",
+        justifyContent: "space-between",
         padding: "10px",
         color: "white",
+        width: "100%",
       }}
     >
       <div>
         <p
           style={{
             color: "white",
-            height: "80px",
-            width: "80px",
+            height: "60px",
+            width: "60px",
             borderRadius: "50%",
             backgroundColor: "#453323",
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
+            margin: "0px",
+            padding: "0px",
           }}
         >
           {<Icon />}
         </p>
       </div>
       <div>
-        <p style={{ color: "white" }}>{title}</p>
-        <p style={{ color: "white" }}>{count}</p>
+        <p style={{ color: "white", margin: "0px", padding: "0px" }}>{count}</p>
+        <p style={{ color: "white", margin: "0px", padding: "0px" }}>{title}</p>
       </div>
     </div>
   );
